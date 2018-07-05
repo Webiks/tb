e06c18e4c7548b77b43f6baf1e38dcf492c9512b
dafnaweiss <dafna@webiks.com>
2018-07-05 23:53:11 +0300

    fix the workspace object in the upload Files

--- server/api/curlMethods.js
+++ server/api/curlMethods.js
@@ -14,7 +14,7 @@ module.exports = function() {
     this.createWorkspaceObject = (workspaceName) => {
         return {
             workspace: {
-                name: `"${workspaceName}"`
+                name: workspaceName
             }
         };
     };

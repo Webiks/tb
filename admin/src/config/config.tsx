8b895c1866a8827e111c401108013acb409b04a0
dafnaweiss <37504470+dafnaweiss@users.noreply.github.com>
2018-07-08 16:54:11 +0300

    fix: base url from server (#5)
    * fix: base url from server
    
    * fix: remove redundant componentWillMount

--- admin/src/config/config.tsx
+++ admin/src/config/config.tsx
@@ -1,14 +1,12 @@
 const config = {
-    ipAddress: 'localhost:',
-    appPort: 3000,
-    serverPort: 4000,
-    baseUrlApi: `http://localhost:4000/api`,
+    baseUrl: {
+       path: 'http://tb-server.webiks.com',
+       api: 'api',
+       login: 'login'
+    },
     authorization: "Basic YWRtaW46Z2Vvc2VydmVy",
     geoserverUserName: 'sdf09rt2s',
-    maxFileSize: 20000 * 1024 * 1024,
-    login: {
-        baseUrl: 'http://localhost:4000/login'
-    }
+    maxFileSize: 20000 * 1024 * 1024
 };
 
 export default config;

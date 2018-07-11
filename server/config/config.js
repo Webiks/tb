2da8a8ff7a106c71eb658c6e06ad1d86085a1156
dafnaweiss <dafna@webiks.com>
2018-07-11 11:22:02 +0300

    server: fix the environment variable by adding .trim() to its value

--- server/config/config.js
+++ server/config/config.js
@@ -10,11 +10,14 @@ module.exports = function(){
         const baseGeoserverImportsUrl = `${config.geoServerPort}/${config.baseUrlGeoserver.restImports}`;
 
         let baseUrl = '';
+        const prod = (process.env.NODE_ENV).trim();
 
-        if (process.env.NODE_ENV === 'production') {
+        if (prod === "production") {
             baseUrl = config.baseUrlRemote;
+            console.log("inside prod: " + baseUrl);
         } else {
             baseUrl = config.baseUrlLocal;
+            console.log("inside else: " + baseUrl);
         }
 
         return {

b56f0c2425028f9d889e2487ff1b74620c999b0a
dafnaweiss <dafna@webiks.com>
2018-07-22 12:25:20 +0300

    fix(server): remove redundent files

--- server/api/index.js
+++ server/api/index.js
@@ -3,11 +3,9 @@ const router = express.Router();
 const worlds = require('./worlds');
 const layers = require('./layers');
 const uploadFiles = require('./uploadFiles');
-const uploadLayers = require('./uploadLayers');
 
 router.use('/worlds', worlds);
 router.use('/layers', layers);
 router.use('/upload', uploadFiles);
-// router.use('/upload', uploadLayers);
 
 module.exports = router;

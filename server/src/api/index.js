9dfec43d12cfbdce769f35adcb8f0954cfddcc32
dafnaweiss <dafna@webiks.com>
2018-08-13 12:00:56 +0300

    add ansyn api (cool)

--- server/src/api/index.js
+++ server/src/api/index.js
@@ -3,9 +3,12 @@ const router = express.Router();
 const dbWorlds = require('./databaseCrud/dbWorlds');
 const dbLayers = require('./databaseCrud/dbLayers');
 const gsUpload = require('./geoserverCrud/gsUpload');
+const ansyn = require('./ansyn');
+
 
 router.use('/dbworlds', dbWorlds);
 router.use('/dblayers', dbLayers);
 router.use('/upload', gsUpload);
+router.use('/ansyn', ansyn);
 
 module.exports = router;

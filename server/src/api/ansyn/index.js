e2ac2bc776808e0ed3b1afde7d134430caf93423
YairTawil <yairtawil@gmail.com>
2018-08-14 12:54:17 +0300

    cr request changes

--- server/src/api/ansyn/index.js
+++ server/src/api/ansyn/index.js
@@ -3,65 +3,37 @@ const router = express.Router();
 const layerModel = require('../../database/schemas/LayerSchema');
 const worldModel = require('../../database/schemas/WorldSchema');
 
-router.post('/fetchLayers', (req, res) => {
-    const {worldName, geometry, dates} = req.body;
-    console.log(worldName, geometry, dates);
-    const {start, end} = dates;
-    const startDate = Date.parse(start);
-    const endDate = Date.parse(end);
-    console.log(startDate + ', ' + endDate);
+const findWorld = ({ name }) => {
+  return worldModel.findOne({ name });
+};
 
-    // 1. find the world
-    const worldObject = worldModel.findOne({name: worldName}, (err, world) => {
-        if (err || !world) {
-            res.send([])
-        } else {
-            console.log('1. found the world: ' + world._id);
-            // 2. find all the world's layers by Id
-            const {layersId} = world;
-            console.log('layersId[0]: ' + layersId[0]);
-            const query = {
-                $or: layersId.map((id) => ({_id: id}))
-            };
-            layerModel.find(query, (err, layers) => {
-                if (err || !layers) {
-                    res.send([]);
-                } else {
-                    console.log('2. found the layers IDs list! length: ' + layers.length);
-                    // 3. find only the layers that are in between the giving dates
-                    console.log("layers[0] date= " + layers[0].date.getTime());
-                    const query = {
-                        date: {
-                            $gte: startDate,
-                            $lte: endDate
-                        }
-                    };
-                    layerModel.find(query, (err, layers) => {
-                        if (err || !layers) {
-                            res.send([]);
-                        } else {
-                            console.log('3. found the match layers by Dates! length: ' + layers.length);
-                            // 4. find only the layers that their footprint polygon is inside the giving geometry
-                            console.log("layers[0] Geometry= " + layers[0].footprint.geometry);
-                            const query = {
-                                'footprint.geometry': {
-                                        $geoWithin: {$geometry: geometry}
-                                    }
-                            };
-                            layerModel.find(query, (err, layers) => {
-                                if (err || !layers) {
-                                    res.send([]);
-                                } else {
-                                    console.log('4. found the match layers by Geometry! length: ' + layers.length);
-                                    res.send(layers);
-                                }
-                            });
-                        }
-                    })
-                }
-            });
-        }
-    });
-});
+const findLayers = (layersId, $gte, $lte, $geometry) => {
+  return layerModel.find({
+    $or: layersId.map((_id) => ({ _id })),
+    date: { $gte, $lte },
+    'footprint.geometry': { $geoWithin: { $geometry } }
+  });
+};
+
+const fetchLayers = (req, res) => {
+  findWorld({ name: req.body.worldName })
+    .then((world) => {
+      if (!world) {
+        throw new Error('No World!');
+      }
+      return world;
+    })
+    .then((world) => {
+      const layers = world.layersId,
+        start = Date.parse(req.body.dates.start),
+        end = Date.parse(req.body.dates.end),
+        geometry = req.body.geometry;
+      return findLayers(layers, start, end, geometry)
+        .then((layers) => res.send(layers));
+    })
+    .catch((err) => res.send([]));
+};
+
+router.post('/fetchLayers', fetchLayers);
 
 module.exports = router;

b53ecb2776514d31fa939fdda45d70eacab51468
dafnaweiss <dafna@webiks.com>
2018-10-08 12:59:45 +0300

    server: add check errors and return an empty files array

--- server/src/api/geoserverCrud/UploadFilesToGS.js
+++ server/src/api/geoserverCrud/UploadFilesToGS.js
@@ -1,9 +1,5 @@
-// const GsLayers  = require("../geoserverCrud/GsLayers");
 require('../fs/fileMethods')();
 require('./curlMethods')();
-// require('../../config/serverConfig')();
-// const configParams = config().configParams;
-// const configUrl = configBaseUrl().configUrl;
 
 class UploadFilesToGS {
 
@@ -12,7 +8,7 @@ class UploadFilesToGS {
 				console.log("uploadFile files: " + JSON.stringify(files));
 				console.log("uploadFile PATH: " + path);
 
-				if (files.length !== 0){
+				if (files.length !== 0) {
 					// 1. create the JSON files with the desire workspace
 					let importJSON = {};
 
@@ -28,52 +24,72 @@ class UploadFilesToGS {
 					let importObj = postImportObj(JSON.stringify(importJSON));
 					console.log("import: " + JSON.stringify(importObj));
 
-					// 3a. for VECTORS only:
-					if (files[0].fileType.toLowerCase() === 'vector') {
-						// check the STATE of each task in the Task List
-						console.log("check the state of each task... ");
-						importObj.tasks.map( task => {
-							console.log(`task ${task.id} state = ${task.state}`);
-							if (task.state !== 'READY') {
-								// get the task object
-								task = getTaskObj(importObj.id, task.id);
-								console.log(`task ${task.id} (before change): ${JSON.stringify(task)}`);
-								// check the state's error and fix it
-								if (task.state === 'NO_CRS') {
-									const updateLayerJson = JSON.stringify(layerSrsUpdate());
-									console.log("NO_CRS updateTaskJson: " + updateLayerJson);
-									// create the update SRS Json files and update the task
-									updateTaskField(updateLayerJson, importObj.id, task.id, 'layer');
-								} else {
-									files = [];
+					if (importObj) {
+						// 3a. for VECTORS only:
+						if (files[0].fileType.toLowerCase() === 'vector') {
+							// check the STATE of each task in the Task List
+							console.log("check the state of each task... ");
+							importObj.tasks.map(task => {
+								console.log(`task ${task.id} state = ${task.state}`);
+								if (task.state !== 'READY') {
+									// get the task object
+									task = getTaskObj(importObj.id, task.id);
+									console.log(`task ${task.id} (before change): ${JSON.stringify(task)}`);
+									// check the state's error and fix it
+									if (task && task.state === 'NO_CRS') {
+										const updateLayerJson = JSON.stringify(layerSrsUpdate());
+										console.log("NO_CRS updateTaskJson: " + updateLayerJson);
+										// create the update SRS Json files and update the task
+										updateTaskField(updateLayerJson, importObj.id, task.id, 'layer');
+									} else {
+										console.log("something is wrong with the file!");
+										files = [];
+									}
 								}
+							});
+							if (files.length !== 0) {
+								files = uploadToGeoserver(importObj.id);
 							}
-						});
-					}
-					// 3b. for RASTERS only: POST the files to the tasks list, in order to create an import task for it
-					else {
-						const rasterTask = sendToTask(path, name, importObj.id);
-						console.log("raster task: " + JSON.stringify(rasterTask));
-						if (!rasterTask) {
-							files = [];
 						}
+						// 3b. for RASTERS only: POST the files to the tasks list, in order to create an import task for it
+						else {
+							const rasterTasks = sendToTask(path, name, importObj.id);
+							console.log("raster tasks: " + JSON.stringify(rasterTasks));
+							if (!rasterTasks) {
+								console.log("something is wrong with the file!");
+								files = [];
+							} else {
+								files = uploadToGeoserver(importObj.id);
+							}
+						}
+					} else {
+						console.log("something is wrong with the JSON file!");
+						files = [];
 					}
 
-					// 4. execute the import task
-					console.log("start executeFileToGeoserver...");
-					executeFileToGeoserver(importObj.id);
+					// return the files
+					console.log("return files: " + JSON.stringify(files));
+					return files;
+				}
 
-					// 5. delete all the uncompleted tasks in the import queue
+				// ============================================= Private Functions =============================================
+				// upload the files to GeoServer
+				function uploadToGeoserver(importObjId) {
+					// 1. execute the import task
+					console.log("start executeFileToGeoserver...");
+					executeFileToGeoserver(importObjId);
+
+					// 2. delete all the uncompleted tasks in the import queue
 					deleteUncompleteImports();
 
-					// 6. remove the files from the local store
+					// 3. remove the files from the local store
 					removeFile(path);
 					// if ZIP files:
 					// send the path in the return files object to remove the zip directory after uploading the layer in geoserver
 					const splitPath = path.split('.');
-					if (splitPath[1] === 'zip'){
-						files.map( file => {
-							if (file.fileType.toLowerCase() === 'vector'){
+					if (splitPath[1] === 'zip') {
+						files.map(file => {
+							if (file.fileType.toLowerCase() === 'vector') {
 								file.splitPath = splitPath[0].trim();
 							} else {
 								file.splitPath = null;
@@ -85,10 +101,8 @@ class UploadFilesToGS {
 						files[0].splitPath = null;
 						console.log("splitPath: " + files[0].splitPath);
 					}
+					return files;
 				}
-				// 7. return the files
-				console.log("return files: " + JSON.stringify(files));
-				return files;
     }
 }
 

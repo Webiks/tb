58fee44882e11a0c6f462eafff7f41e4c6b8109e
YairTawil <yairtawil@gmail.com>
2018-11-04 18:15:03 +0200

    refactor: admin only

--- server/src/api/ansyn/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const fetchLayers = require('./fetchLayers');
-const uploadFiles = require('../upload/uploadFiles');
-
-router.post('/fetchLayers', fetchLayers);
-router.use('/uploadImage', uploadFiles);
-
-module.exports = router;

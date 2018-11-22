3d14da15fccd57f34aae9bc45e541a8c6e0426bf
Dafna Weiss <37504470+dafnaweiss@users.noreply.github.com>
2018-11-22 15:02:55 +0200

    fix: add fields to the imageMetaData interface (#59)

--- src/interfaces/IImageMetaData.tsx
+++ src/interfaces/IImageMetaData.tsx
@@ -9,49 +9,22 @@ export interface IImageMetaData {
     GPSLongitudeRef: string;
     GPSLongitude: number;
     GPSAltitude: number;
+    relativeAltitude: number;
     ExifImageWidth: number;
     ExifImageHeight: number;
+    pitch: number,
+    yaw: number,
+    roll: number,
+    cameraPitch: number,
+    cameraYaw: number,
+    cameraRoll: number,
     gimbalRollDegree: number,
     gimbalYawDegree: number,
     gimbalPitchDegree: number,
     flightRollDegree: number,
     flightYawDegree: number,
-    flightPitchDegree: number
-    // ImageDescription?: string;
-    // SerialNumber?: string;
-    // InteropIndex?: string;
-    // Orientation: number;
-    // XResolution: number;
-    // YResolution: number;
-    // ResolutionUnit: number;
-    // Software?: string;
-    // YCbCrPosition: number;
-    // GPSVersionId: [number, number, number, number];
-    // ExposureTime?: number;
-    // ExposureProgram?: number;
-    // ExposureCompensation?: number;
-    // ExposureIndex?: string;
-    // ExposureMode?: number;
-    // FNumber?: number;
-    // ISO?: number;
-    // CompressedBitPerPixel?: number;
-    // ShutterSpeedValue?: number;
-    // ApertureValue?: number;
-    // MaxApertureValue?: number;
-    // SubjectDistance?: number;
-    // SubjectDistanceRange?: number;
-    // MeteringMode?: number;
-    // LightSource?: number;
-    // Flash?: number;
-    // FocalLength?: number;
-    // FocalLengthIn35mmFormat?: number;
-    // ColorSpace?: number;
-    // CustumRendered?: number;
-    // WhiteBalance?: number;
-    // DigitalZoomRatio?: string;
-    // SceneCaptureType?: number;
-    // GainControl?: number;
-    // Contrast?: number;
-    // Saturation?: number;
-    // Sharpness?: number;
+    flightPitchDegree: number,
+    camReverse: number,
+    gimbalReverse: number,
+    fieldOfView: number
 }
